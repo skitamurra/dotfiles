ae86cfd157661a9dc20d0ae76937e48fa517599f
skitamurra <s.g004baa@gmail.com>
2025-12-31 10:41:43 +0900

    improve quit cmd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -76,6 +76,22 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    local current_win = vim.api.nvim_get_current_win()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].buftype == '' then
+          return
+        end
+      end
+    end
+    vim.cmd.only({ bang = true })
+  end,
+  desc = 'Close all special buffers and quit Neovim',
+})
+
 require("plugins")
 require("keymaps")
 require("config.util")
