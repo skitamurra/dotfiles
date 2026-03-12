0483c8f85e696f2c45a5b3c296b32118d5765d23
sg004baa <s.g004baa@gmail.com>
2026-03-12 10:00:38 +0900

    refactor: Update skkeleton config and improve wezterm keybindings

--- .config/nvim/lua/config/skkeleton.lua
+++ .config/nvim/lua/config/skkeleton.lua
@@ -3,17 +3,18 @@ vim.api.nvim_create_autocmd('User', {
   callback = function()
     vim.fn['skkeleton#azik#add_table']('us')
     vim.fn['skkeleton#config']({
-      kanaTable           = 'azik',
-      sources             = { 'skk_server' },
-      globalDictionaries  = {
-        '~/.config/ibus-skk/abbr.dict',
+      kanaTable = 'azik',
+      sources = { 'skk_server', 'user_dictionary', 'global_dictionary' },
+      serverHost = 'localhost',
+      serverPort = '1178',
+      globalDictionaries = {
+        { vim.fn.expand("~/.skk/SKK-JISYO.L"),    "euc-jp" },
+        { vim.fn.expand("~/.skk/SKK-JISYO.lisp"), "euc-jp" },
       },
-      userDictionary      = '~/.config/ibus-skk/user.dict.utf8',
+      userDictionary = '~/.config/ibus-skk/user.dict.utf8',
       completionRankFile  = '~/.config/ibus-skk/rank.json',
-      eggLikeNewline      = true,
+      eggLikeNewline = true,
+      keepState = false,
     })
   end,
 })
-
-vim.keymap.set({ 'i', 'c' }, '<C-j>', '<Plug>(skkeleton-enable)',  { noremap = false })
-vim.keymap.set({ 'i', 'c' }, 'L',     '<Plug>(skkeleton-disable)', { noremap = false })
