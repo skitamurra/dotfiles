7564c356bc6c2e408653b8247a9c9b55f70857af
skitamurra <s.g004baa@gmail.com>
2026-03-22 10:15:51 +0900

    refactor(wezterm): refactor WezTerm config by removing unused settings and comments

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
 local cfg_file = wezterm.config_file
-config.automatically_reload_config = true
-
 local cfg_dir  = cfg_file:gsub("[^/\\]+$", ""):gsub("\\", "/")
 package.path = table.concat({
   cfg_dir .. "?.lua",
@@ -14,20 +11,15 @@ config.leader = { key = " ", mods = "SHIFT", timeout_milliseconds = 2000 }
 config.disable_default_key_bindings = true
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
-
 config.automatically_reload_config = true
-config.use_ime = true
 config.default_cursor_style = "BlinkingBar"
 config.default_prog = { "wsl", "~", "zsh" }
-config.unix_domains = {{ name = 'idle' }}
-
 config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font_with_fallback({
   { family = 'HackGen Console', weight = 'Regular' },
   { family = 'SauceCodePro Nerd Font Mono' }
 })
 config.font_size = 13.0
-
 config.window_frame = { active_titlebar_bg = "none" }
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
@@ -40,50 +32,17 @@ config.background = {
     height = "100%",
   }
 }
-
 config.show_new_tab_button_in_tab_bar = false
 config.show_close_tab_button_in_tabs = false
-
 config.inactive_pane_hsb = {
   saturation = 0.4,
   brightness = 0.4,
 }
-
 config.colors = {
   tab_bar = {
     inactive_tab_edge = "none",
   },
 }
-
--- wezterm.on('gui-startup', function (cmd)
---   -- tab1
---   local tab1, pane1, window = wezterm.mux.spawn_window(cmd or {})
---   pane1:send_text("note\n")
---   window:gui_window():maximize()
---
---   -- tab2
---   local _, root = window:spawn_tab({})
---   local left_top = root
---   local right_top = root:split({
---     direction = "Right",
---     size = 0.5,
---   })
---   left_top:send_text("cd docker\n")
---   right_top:send_text("cd auth\n")
---   left_top:split({
---     direction = "Bottom",
---     size = 0.5,
---   })
---   right_top:split({
---     direction = "Bottom",
---     size = 0.5,
---   })
---
---   -- tab3
---   window:spawn_tab({})
---
---   tab1:activate()
--- end)
 
 require("workspace").apply_to_config(config)
 require("tab").apply_to_config(config)
