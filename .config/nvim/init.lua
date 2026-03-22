6f0a9d7e153f5ec37a4ed4971917cbd884583b9d
skitamurra <s.g004baa@gmail.com>
2026-03-22 12:38:07 +0900

    refactor(nvim): move autocmd definitions to a separate file for clarity

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,59 +63,8 @@ vim.diagnostic.config {
   },
 }
 
-vim.api.nvim_create_autocmd("BufRead", {
-  callback = function()
-    local git_root = require("config.util").get_git_root()
-    if git_root then
-      vim.cmd("lcd " .. git_root)
-    else
-      vim.cmd("lcd %:h")
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  callback = function()
-    vim.cmd("wincmd L | :vert resize 80")
-  end,
-})
-
-vim.api.nvim_create_autocmd('QuitPre', {
-  callback = function()
-    local current_win = vim.api.nvim_get_current_win()
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      if win ~= current_win then
-        local buf = vim.api.nvim_win_get_buf(win)
-        if vim.bo[buf].buftype == '' then
-          return
-        end
-      end
-    end
-    vim.cmd.only({ bang = true })
-  end,
-  desc = 'Close all special buffers and quit Neovim',
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.rs" },
-      callback = function()
-        vim.lsp.buf.format({
-          buffer = ev.buf,
-          filter = function(f_client)
-            return f_client.name ~= "null-ls"
-          end,
-          async = false,
-        })
-      end,
-    })
-  end,
-})
-
 require("plugins")
 require("keymaps")
+require("autocmds")
 require("config.util")
 require("lsp")
