3d194460185311704a7087fa835285c77cd957ef
skitamurra <s.g004baa@gmail.com>
2026-04-01 08:18:12 +0900

    fix(nvim): update nvim to 0.12.0

--- .config/nvim/lua/lsp/lua_ls.lua
+++ .config/nvim/lua/lsp/lua_ls.lua
@@ -1,5 +1,11 @@
 return {
   cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
+  filetypes = { 'lua' },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.root(fname, { '.git' })
+    on_dir(root or vim.fn.fnamemodify(fname, ':h'))
+  end,
   init_options = {
     lspMux = {
       version = "1",
--- .config/nvim/lua/lsp/pyright.lua
+++ .config/nvim/lua/lsp/pyright.lua
@@ -1,6 +1,7 @@
 local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
 local pyright_config = {
-  cmd = { mason_bin .. "/pyright-langserver", "--stdio" }
+  cmd = { mason_bin .. "/pyright-langserver", "--stdio" },
+  filetypes = { 'python' },
 }
 
 local function detect_venv(root)
