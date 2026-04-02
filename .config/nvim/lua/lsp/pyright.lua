0c60e022c1f0d060612db7afc7e3ae82dd515011
sg004baa <s.g004baa@gmail.com>
2026-04-02 10:00:49 +0900

    fix(nvim): Add root markers for Pyright configuration in Neovim

--- .config/nvim/lua/lsp/pyright.lua
+++ .config/nvim/lua/lsp/pyright.lua
@@ -2,6 +2,7 @@ local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
 local pyright_config = {
   cmd = { mason_bin .. "/pyright-langserver", "--stdio" },
   filetypes = { 'python' },
+  root_markers = { "Pipfile", ".git" }
 }
 
 local function detect_venv(root)
